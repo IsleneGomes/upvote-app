94aa86a4445f189907ad28b54f79d4051a004eb5
IsleneGomes <islenegomes@gmail.com>
2021-10-30 21:59:25 -0300

    Adiciona imagem do login

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import logotipo from '../components/download.jpeg'
 
 export default function Header() {
   return (
     <header className="header">
         <div className="toolbar">
           <div className="">
-            <a href='/'>Logo</a>
+            <a href='/'>
+            <img src={ logotipo } alt='logotipo da Segware do Brasil' width='50px' />
+            </a>
           </div>
           <div className="">
             <button>Novo Post</button>
